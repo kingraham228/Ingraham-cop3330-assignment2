7696328fb94708c995f8c3c0b951bce5b34ad283
kingraham228 <kate.ingraham@knights.ucf.edu>
2021-06-13 16:58:19 -0400

    Clean up comments

--- src/main/java/ex24/Anagram.java
+++ src/main/java/ex24/Anagram.java
@@ -3,19 +3,7 @@ package ex24;
  *  UCF COP3330 Summer 2021 Assignment 2 Solution
  *  Copyright 2021 Kate Ingraham
  */
-/*Create a program that compares two strings and determines if the two strings are anagrams.
-The program should prompt for both input strings and display the output as shown in the example that follows.
 
-Example Output
-Enter two strings and I'll tell you if they are anagrams:
-Enter the first string: note
-Enter the second string: tone
-"note" and "tone" are anagrams.
-
-Constraints
-Implement the program using a function called isAnagram, which takes in two words as its arguments and returns true or false.
-Invoke this function from your main program.
-Check that both words are the same length.*/
 
 import java.util.Arrays;
 import java.util.Scanner;
